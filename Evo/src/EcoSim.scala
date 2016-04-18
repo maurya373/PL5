05cb37ba86825831bdca3e0f39f34e49e1abae2b
Gilad Oved <giladoved@gmail.com>
2016-04-17 20:15:50 -0500

    can edit species properties

--- Evo/src/EcoSim.scala
+++ Evo/src/EcoSim.scala
@@ -3,37 +3,50 @@ import scala.collection.mutable.Map
 object EcoSim {
   
   class Species {
-    var name: String = null
-    var population: Int = 0
-    var growthrate: Double = 0.0
-    var starttime: Int = 0
+    var _name: String = null
+    var _population: Int = 0
+    var _growthrate: Double = 0.0
+    var _starttime: Int = 0
     
     def called(n: String) = {
-      name = n
+      _name = n
       GlobalVars.addSpecie(this)
       this
     }
     
     def of(i: Int) = {
-      population = i
+      _population = i
       this
     }
     
     def growat(x: Double) = {
-      growthrate = x
+      _growthrate = x
       this
     }
     
     def startingat(t: Int) = {
-      starttime = t
+      _starttime = t
       this
     }
     
     def show() {
-      println("name: " + name)
-      println("population: " + population)
-      println("growthrate: " + growthrate)
-      println("starttime: " + starttime)
+      println("name: " + _name)
+      println("population: " + _population)
+      println("growthrate: " + _growthrate)
+      println("starttime: " + _starttime)
+    }
+    
+    
+    def population(x: Int) {
+      _population = x
+    }
+   
+    def growthrate(x: Double) {
+      _growthrate = x
+    }
+     
+    def starttime(x: Int) {
+      _starttime = x
     }
   }
   
@@ -47,7 +60,7 @@ object EcoSim {
     var species = Map[String, Species]()
     
     def addSpecie(s: Species) {
-      species += (s.name -> s)
+      species += (s._name -> s)
     }
     
     def doesExist(name: String): Species = {
@@ -61,9 +74,11 @@ object EcoSim {
   
   def main(args: Array[String]) = {
     new Species called "Frog" of 1000 growat .4 startingat 0
-    "Frog" show
+    "Frog" population 10
+    "Frog" growthrate .3
+    "Frog" starttime 2
     
-    //"Frog" population is 10
+    "Frog" show
   }
   
 }
