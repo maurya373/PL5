d45248e73a1c55af085f55a1111d3146344eb592
Gilad Oved <giladoved@gmail.com>
2016-04-30 01:57:52 -0500

    more of maurya changes included

--- Evo/src/Evo.scala
+++ Evo/src/Evo.scala
@@ -1,4 +1,5 @@
 import collection.mutable.Buffer
+import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Map
 
 object Evo {
@@ -19,6 +20,8 @@ object Evo {
     var deterministicEvents = Map[Symbol, DeterministicEvent]()
     //all random events
     var randomEvents = Map[Symbol, RandomEvent]()
+    //all generic events
+    var genericEvents = Map[Symbol, GenericEvent]()
 
     //ADD METHODS
     //add a new species to the ecosystem
@@ -35,6 +38,12 @@ object Evo {
     //add a new random event
     def addRandomEvent(e: RandomEvent) {
       randomEvents += (e._name -> e)
+      events += (e._name -> e)
+    }
+    
+    //add a new generic event
+    def addGenericEvent(e: GenericEvent) {
+      genericEvents += (e._name -> e)
       events += (e._name -> e)
     }
 
@@ -57,6 +66,12 @@ object Evo {
       else null
     }
 
+    //get a generic event
+    def getGenericEvent(name: Symbol): GenericEvent = {
+      if (genericEvents.contains(name)) genericEvents(name)
+      else null
+    }
+    
     //get a species from a name
     def getSpecies(name: Symbol): Species = {
       if (species.contains(name)) species(name)
@@ -84,6 +99,8 @@ object Evo {
       species.keys.foreach((sp) =>
         species(sp).update(d))
     }
+    
+    val DoNothing: Symbol = 'DoNothing
 
     //SHOW METHODS
     //print out all random events and deterministic events
@@ -129,6 +146,41 @@ object Evo {
     }
   }
 
+  //
+  def predation() {
+    var updatedSpecies = Map[Symbol, Long]()
+    // loop through all species
+    EcoSystem.species.keys.foreach((sp) =>
+
+      // loop through prey of each species
+      sp.prey.keys.foreach((pr) => {
+
+        if (EcoSystem.species(pr)._population == 0) {
+          println(pr + " is extinct ******")
+        } else {
+          println(pr + " is not extinct")
+          var deathNum = sp.prey(pr) * EcoSystem.species(sp)._population
+
+          //println(sp._population + " " + sp + " ate " + deathNum + " " + pr)
+
+          if (updatedSpecies.contains(pr)) {
+            var v = Math.max(updatedSpecies(pr) - deathNum, 0)
+            updatedSpecies.put(pr, v)
+          } else {
+            var v = Math.max(EcoSystem.species(pr)._population - deathNum, 0)
+            updatedSpecies.put(pr, v)
+          }
+        }
+      }))
+
+    EcoSystem.species.keys.foreach((sp) =>
+
+      if (updatedSpecies.contains(sp)) {
+        sp._population = updatedSpecies(sp)
+      })
+
+  }
+
   /********* Classes ***********/
   //SPECIES
   class Species {
@@ -140,9 +192,16 @@ object Evo {
     var _deathrate: Double = 0.0
     var _carryingcapacity: Long = Long.MaxValue
     
+    //
+    var _traits : ArrayBuffer[Map[String, Double]] = null
+    var _traitReference : Map[String, Int] = null 
+    var _currentTrait : String = null
+    
     //prey of the species
     var prey = Map[Symbol, Long]()
+    var preyEvent = Map[Symbol, (Long, String)]()
 
+    
     //SETTERS
     //setter for the _name property
     def called(n: Symbol) = {
@@ -157,7 +216,7 @@ object Evo {
       else { _population = p }
       this
     }
-    
+
     //setter for the _carryingcapacity property
     def carryingcapacity(cc: Long) = {
       _carryingcapacity = cc
@@ -224,6 +283,18 @@ object Evo {
             })
         println()
       }
+      if(this._traitReference != null){
+        var currentIndex = 0
+        this._traitReference.keys.foreach{ i =>
+          currentIndex = this._traitReference.apply(i)
+          println("Trait: "+ i)
+          var currentMap = this._traits.apply(currentIndex)
+          currentMap.keys.foreach{ i =>  
+            print( "Phenotype = " + i )
+            println(" Occurence = " + currentMap(i) )
+          }
+        }
+      }
     }
 
     //print name and population
@@ -235,27 +306,101 @@ object Evo {
     def update(t: Int) = population(grow(t))
     //updates population based on percentage
     def update(t: Double) = population(grow(t))
-    
+
     //grows the population by growth rate for duration time t  
     private def grow(t: Int): Long =
       if (t > 0) (_population + ((_population * _birthrate).toLong) - ((_population * _deathrate).toLong))
       else grow(t - 1)
-    
+
     //grows the population based on a percentage
     private def grow(t: Double): Long =
       if (t > 0) ((_population * t).toLong)
       else grow(t - 1)
 
-    //
     def setAsPrey(s: Symbol, consumption: Long) {
-        if (!EcoSystem.speciesExists(s)) println(s + " is extinct *****")
-        else prey += (s -> consumption)
+        
+        if (!EcoSystem.genericEvents.contains(EcoSystem.DoNothing)) {
+          
+          new GenericEvent called EcoSystem.DoNothing definedAs (new FUNCTION {
+            new PRINT("Nothing should happen")
+          })
+        }
+        
+//        if (!speciesExists(s)) {
+//           println(s + " is extinct *****")
+//        }
+        else {
+           prey += (s -> consumption)
+        }
+    }
+    
+    def setAsPrey(s: Symbol, consumption: Long, ev: String) {
+        if (!EcoSystem.species.contains(s)) {
+           println(s + " is extinct *****")
+        }
+        else {
+           prey += (s -> consumption)
+        }
+    }
+    
+    def setAsPredator(s: Symbol, consumption: Long) {
+        if (!EcoSystem.species.contains(s)) {
+           println(s + " is extinct *****")
+        }
+        else {
+           EcoSystem.species(s).prey += (_name -> consumption);
+        }
     }
     
     //
-    def setAsPredator(s: Symbol, consumption: Long) {
-        if (!EcoSystem.speciesExists(s)) println(s + " is extinct *****")
-        else EcoSystem.species(s).prey += (_name -> consumption)
+    def addTrait(traitName : String)={
+      if(this._traits == null){
+        //Init traits list and add this map
+        this._currentTrait = traitName
+        this._traitReference = Map[String,Int](traitName -> 0)
+        this._traits = new ArrayBuffer[Map[String, Double]]
+        this
+        //phenotype calls will now add actual phenotypes to the appropriate Map in _traits
+      }
+      else{
+        this._currentTrait = traitName
+        var newIndex = this._traits.size
+        this._traitReference += (traitName -> newIndex)
+        this
+        //phenotype calls will now add actual phenotypes to the appropriate Map in _traits
+      }
+    }
+    
+    //
+    def phenotype(pheno : String, occurence : Double)={
+      //Get index of current trait
+      var currentIndex = this._traitReference.apply(this._currentTrait)
+      //If there is a map at this index, add to it
+      if(this._traits.isDefinedAt(currentIndex)){
+        //May need to create map?
+        var tempMap = this._traits.apply(currentIndex)
+        tempMap.+=(pheno -> occurence)
+        this._traits.update(currentIndex, tempMap)
+        this
+        //Okay, added phenotype.
+      }
+      else{
+        //CurrentIndex does not exist so need to create map.
+        var newMap = Map[String, Double](pheno -> occurence)
+        this._traits.insert(currentIndex, newMap)
+        this
+        //Added phenotype.
+      }
+    }
+    
+    //
+    def showTrait(traitName: String){
+      var currentIndex = this._traitReference.apply(traitName)
+      println("Trait: "+ traitName)
+      var currentMap = this._traits.apply(currentIndex)
+      currentMap.keys.foreach{ i =>  
+        print( "Phenotype = " + i )
+        println(" Occurence = " + currentMap(i) )}
     }
   }
 
@@ -333,7 +478,6 @@ object Evo {
     //implement execute function from EventClass
     def execute() {
       var function = EcoSystem.functions(_name)
-
       function.get
     }
 
@@ -347,6 +491,30 @@ object Evo {
       EcoSystem.functions += (_name -> new functionWrapper(function))
     }
   }
+  
+  class GenericEvent extends EventClass {
+    
+    // Setter for event name
+    def called(n: Symbol) = {
+      _name = n
+      EcoSystem.addGenericEvent(this)
+      this
+    } 
+    
+    def execute() {
+      var function = EcoSystem.functions(_name)
+      function.get
+    } 
+    
+    //add the function to the global map of functions (using call-by-name)
+    def definedAs(function: => FUNCTION) {
+      EcoSystem.functions += (_name -> new functionWrapper(function))
+    }
+    
+    def at(t: Int) = {
+      this
+    }
+}
 
   /********* Implicits ***********/
   //try to convert any Symbol to an event type
@@ -396,6 +564,8 @@ object Evo {
           }
         })
 
+      predation()
+        
       //increment the time
       EcoSystem.worldTime += 1
       println(); println()
@@ -446,7 +616,7 @@ object Evo {
   class UPDATE_POPULATION(species: Symbol, p: Long) extends Expression {
     species population p
   }
-  
+
   //Evo - updates all populations to a specific number
   class UPDATE_ALL_POPULATIONS_TO(i: Int) extends Expression {
     EcoSystem.updatePopulations(i)
@@ -469,9 +639,9 @@ object Evo {
     new Species called 'Cricket of 500 birthrate 0 deathrate 0 at 0
 
     //set food web relations between species 
-    'Frog setAsPrey('Fly, 5)
-    'Frog setAsPrey('Cricket, 2)
-    
+    'Frog setAsPrey ('Fly, 5)
+    'Frog setAsPrey ('Cricket, 2)
+
     //example of defining deterministic event
     new DeterministicEvent called 'Tornado at 4 definedAs (new FUNCTION {
       new PRINT("FRAIJ")
@@ -482,7 +652,7 @@ object Evo {
         new UPDATE_POPULATION('Pig, 696969)
       ))
       new IF(('Pig getPopulation) < ('Frog getPopulation), (
-        new UPDATE_ALL_POPULATIONS_BY(.2)
+        new UPDATE_ALL_POPULATIONS_BY(.2)  
       ))
       new STEP(3, (
         new PRINT("FARES")
@@ -493,7 +663,7 @@ object Evo {
     new RandomEvent called 'Wipeout withProbability .5 definedAs (new FUNCTION {
       new PRINT("WIPEOUT!!")
     })
-    
+
     //run the ecosystem - print the states before and after
     showEcosystem
     simulate(5)
